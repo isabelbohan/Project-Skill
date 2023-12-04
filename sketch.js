bbd62187e763699f56f433a74f6c7615d5439a2e
isabelbohan <152305166+isabelbohan@users.noreply.github.com>
2023-12-04 16:55:46 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,57 +2,31 @@
  * 2023-12-5
  */
 
-let flowers = [];
-
 function setup() {
   createCanvas(400, 400);
-  
-  // Create 20 flowers
-  for(let i = 0; i < 20; i++){
-    flowers.push(new Flower());  
-  }
 }
 
 function draw() {
   background(220);
-  
-  // Display and move each flower
-  for(let flower of flowers){
-    flower.display();
-    flower.move();
-  }
-}
 
-// Flower class
-class Flower {
-  
-  constructor(){
-    // Random x,y position
-    this.x = random(0, width);
-    this.y = random(0, height);
-    // Size from 50-100
-    this.size = random(50, 100);
-    // Random movement speed
-    this.speed = random(1, 3);
+  // Stem
+  fill(34, 139, 34); // Green color
+  rect(195, 200, 10, 100);
+
+  // Flower head
+  fill(255, 165, 0); // Orange color
+  ellipse(200, 150, 60, 60);
+
+  // Petals
+  fill(255, 255, 0); // Yellow color
+
+  // Draw petals using a loop
+  for (let i = 0; i < 6; i++) {
+    let angle = map(i, 0, 6, 0, TWO_PI); // Distribute petals evenly around the flower
+    let petalX = 200 + 20 * cos(angle);
+    let petalY = 150 + 20 * sin(angle);
+    ellipse(petalX, petalY, 20, 40);
   }
-  
-  // Draw flower
-  display(){
-    push();
-    translate(this.x, this.y);
-    noStroke();
-    fill('pink');
-    ellipse(0, 0, this.size);
-    pop();
-  }
-  
-  // Move flower randomly
-  move(){ 
-    this.x += random(-this.speed, this.speed);
-    this.y += random(-this.speed, this.speed);
-  }
-  
-}
 
   // Flower center
   fill(255, 0, 0); // Red color
